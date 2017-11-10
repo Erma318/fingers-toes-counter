d5eb6e96c54c0b789dd03d72d7e4f0c29c05eab9
Erma318 <fengyz318@gmail.com>
2017-11-10 00:22:50 -0600

    update logic and validation
    change the logic of three button, and add positive number validation.

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ fingers-toes-counter
 This is a mini project containing a signal page do the counter of whether fingers or toes.
 ## Details
 The page has the following elements:
-
-1. An input field that will take a number. Then there is a counter counting up incrementally every second and the counter will stop once it reaches the given number.
-2. Have 2 blocks one with the word "fingers" and the other with the word "toes". Highlight fingers when the counter is divisible by 3. Highlight toes when the counter is divisible by 5. Highlight fingers and toes when the counter is divisible by both 3 and 5.
-3. A reset button to reset the counter and clear the input.
-4. A restart button to restart the counter.
+1. An input field that will take a number. The input number should be **positive**. Otherwise the page will show **alert** telling the user to revise the input.
+2. A counter counting up incrementally every second and the counter will stop once it reaches the given number.
+3. Have 2 blocks one with the word "fingers" and the other with the word "toes". Highlight fingers when the counter is divisible by 3. Highlight toes when the counter is divisible by 5. Highlight fingers and toes when the counter is divisible by both 3 and 5.
+4. A reset button to reset the counter and clear the input.
+5. A restart button to restart the counter.
+6. **When the counter is running, the input area can't be edited**, only if the reset button is clicked.
--- counter.js
+++ counter.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
   count = 0;
   myCount = null;
-
+  count_end = null;
+  flag = false;
+  //after click the set button, the timing event for counter will be started
   $("#set").click(function(){
-    if ( $("#number_set").val() != "") {
+    if (/^\+?[1-9][0-9]*$/.test($("#number_set").val())) {
       count_end = $("#number_set").val();
       clearInterval(myCount);
       myCount = setInterval(function(){myTimer()},1000);
+      flag = true;
+    } else {
+        window.alert("Please input a positive number.")
     }
   });
-
+ //the timing event for the counter
   function myTimer(){
+    $("#number_set").attr("readonly",true);
     if (count_end == "") {
       clearInterval(myCount);
       return;
@@ -37,8 +43,12 @@ $(document).ready(function(){
     }
     count = count + 1;
   }
-
+  //after click the set button, the timing event for counter will be stoped
   $("#reset").click(function(){
+    if (!flag) {
+      return;
+    }
+    $("#number_set").attr("readonly", false);
     document.getElementById("number_set").value = "";
     count = 0;
     count_end = null;
@@ -46,17 +56,16 @@ $(document).ready(function(){
     clearInterval(myCount);
     $(".fingers").removeClass("highlight");
     $(".toes").removeClass("highlight");
+    flag = false;
   });
-
+  //after click the set button, the timing event for counter will be stoped then restarted
   $("#restart").click(function(){
-    count = 0;
-    //count_end = $("#number_set").val();
-    if ($("#number_set").val() == "") {
+    if (!flag) {
       return;
     }
-    //document.getElementById("count_show").innerHTML=count;
+    count = 0;
     clearInterval(myCount);
     myCount = setInterval(function(){myTimer()},1000);
+    flag = true;
   });
-
 });
